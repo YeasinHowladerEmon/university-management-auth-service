38292b72987f338ee0514a250edb7102e0b7779b
emonibnsalim <emonibnsali@gmail.com>
2023-05-31 09:56:32 +0600

    [done]

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function boostrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`database is connected successfully`);
+        app.listen(config.port, () => {
+            console.log(`Application listening at http://localhost:${config.port}`)
+        })
+    } catch (error) {
+        console.log(`failed to connect database`, error);
+    }
+}
+boostrap()
