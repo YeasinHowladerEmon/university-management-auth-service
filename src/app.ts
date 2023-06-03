c31c791a67b4ae74a6fec4a05f6e2c2f9084d7a5
emonibnsalim <emonibnsali@gmail.com>
2023-06-03 12:08:43 +0600

    [done] setup linteg custom

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRouter from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -9,8 +10,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully!')
+//Application
+
+app.use('/api/v1/users/', userRouter)
+
+// testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('working successfully')
 })
 
 export default app
