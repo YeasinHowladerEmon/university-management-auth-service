0e88fa1e24c03a13496d79683be2f8d79cbfa459
emonibnsalim <emonibnsali@gmail.com>
2023-06-04 03:44:13 +0600

    install logger and setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    // console.log(`database is connected successfully`)
+    logger.info(`database is connected successfully`)
     app.listen(config.port, () => {
-      // console.log(`Application listening at http://localhost:${config.port}`)
+      logger.info(`Application listening at http://localhost:${config.port}`)
     })
   } catch (error) {
-    // console.log(`failed to connect database`, error)
+    logger.error(`failed to connect database`, error)
   }
 }
 boostrap()
