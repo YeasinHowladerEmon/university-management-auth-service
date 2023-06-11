e67b62eaa7533e7e043b253bab2bc1b9befe78cc
emonibnsalim <emonibnsali@gmail.com>
2023-06-11 15:53:20 +0600

    [done] module-13

--- src/app/middlewares/requestValidation.ts
+++ src/app/middlewares/requestValidation.ts
@@ -1,20 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-// import { UserValidation } from '../modules/users/user.validation'
 import { AnyZodObject } from 'zod';
 
-// const validateRequest: RequestHandler = async (req, res, next) => {
-//   try {
-//     await UserValidation.createUserZodSchema.parseAsync({
-//       body: req.body,
-//       query: req.query,
-//       params: req.params,
-//       cookies: req.cookies,
-//     })
-//     return next()
-//   } catch (error) {
-//     next(error)
-//   }
-// }
 const validateRequest =
   (schema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
