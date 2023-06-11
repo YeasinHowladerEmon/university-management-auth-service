e66f3c139e11a4dc6922c537e879435cdb8ff961
emonibnsalim <emonibnsali@gmail.com>
2023-06-11 09:20:24 +0600

    [Done]optimize routes others module-13-13

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,18 +1,18 @@
-import { createLogger, format, transports } from 'winston'
-import path from 'path'
+import { createLogger, format, transports } from 'winston';
+import path from 'path';
 
-const { combine, timestamp, label, printf } = format
-import DailyRotateFile from 'winston-daily-rotate-file'
+const { combine, timestamp, label, printf } = format;
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 //custom log formate
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
-  const date = new Date(timestamp)
-  const hour = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
-  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
-})
+  const date = new Date(timestamp);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
+});
 
 const logger = createLogger({
   level: 'info',
@@ -39,7 +39,7 @@ const logger = createLogger({
       maxFiles: '14d',
     }),
   ],
-})
+});
 const errorLogger = createLogger({
   level: 'error',
   format: combine(
@@ -65,5 +65,5 @@ const errorLogger = createLogger({
       maxFiles: '14d',
     }),
   ],
-})
-export { logger, errorLogger }
+});
+export { logger, errorLogger };
